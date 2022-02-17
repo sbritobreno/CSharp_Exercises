4e7f21bcff9fcbac4bbc7e2661ff693758b64795
Breno Silva Brito <90871295+sbritobreno@users.noreply.github.com>
2022-02-17 21:02:07 +0000

    Add files via upload

---
+++ Exercise_week4/exercise_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_week4
+{
+//    1. Create a console-based application whose Main() method asks the user to input an integer
+//and then calls a method named MultiplicationTable(), which displays the results of
+//multiplying the integer by each of the numbers 2 through 10.
+    public class exercise_1
+    {
+        public static void MultiplicationTable(int number)
+        {
+            int i = 2;
+            while(i <= 10)
+            {
+                var result = number * i;
+                Console.WriteLine($"{number} x {i} = {result}");
+                i++;    
+            }
+        }
+    }
+}
---
+++ Exercise_week4/exercise_2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_week4
+{
+//    2. Create a console-based program whose Main() method prompts the user for an integer
+//value and, in turn, passes the value to a method that squares the number and to a method
+//that cubes the number.The Cube() method should call the Square() method. The Main()
+//method displays the results returned from each of the other methods.
+    public class exercise_2
+    {
+        public static void Cube(int number)
+        {
+            Console.WriteLine($"The cube of {number} is " + number * number * number);
+            Square(number);
+        }
+        public static void Square(int number)
+        {
+            Console.WriteLine($"The square of {number} is " + number * number);
+        }
+    }
+}
